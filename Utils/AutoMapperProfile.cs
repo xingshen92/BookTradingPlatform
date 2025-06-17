57f49347c09d924657d9d123f73fd33d0caea924
xs <enhdanny92@gmail.com>
2025-06-17 16:34:24 +0800

    新增 AutoMapper 配置以支持 ProductCreateDto 和 Product 之間的映射

--- Utils/AutoMapperProfile.cs
+++ Utils/AutoMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using BookTradingPlatform.Controllers.Models;
 using BookTradingPlatform.Dtos;
 using BookTradingPlatform.Models;
 
@@ -8,7 +9,15 @@ namespace BookTradingPlatform.Utils
     {
         public AutoMapperProfile()
         {
+            // User → 註冊回應 DTO
             CreateMap<User, RegisterResponseDto>();
+            // Dto → Entity
+            CreateMap<ProductCreateDto, Product>()
+                .ForMember(dest => dest.User, opt => opt.Ignore()); // 忽略 User 屬性，因為在服務層會設定
+            // Entity → VO (回傳前端)
+            CreateMap<Product, ProductVO>();
+            // Entity ↔ BO (商業邏輯層)
+            CreateMap<Product, ProductBO>().ReverseMap();
         }
     }
 }
